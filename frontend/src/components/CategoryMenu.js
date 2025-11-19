a9d3d4ee443c08674fb8f84693fbfa84d7639a40
hominlll <simbalhm@gmail.com>
2025-11-19 11:00:30 +0900

    카테고리 이미지수정

--- frontend/src/components/CategoryMenu.js
+++ frontend/src/components/CategoryMenu.js
@@ -2,24 +2,24 @@ import React, { useState } from 'react';
 import './CategoryMenu.css';
 
 const categories = [
-  '전체','AI 개발','AI 활용(AX)','개발·프로그래밍','게임 개발',
-  '데이터 사이언스','보안·네트워크','하드웨어','디자인·아트',
-  '기획·경영·마케팅','모의면접 or 취준 관련','이력서'
+  '전체','경영','경제','교육','기계',
+  '디자인','법','생명','전기·전자',
+  '컴공','화학','면접'
 ];
 
 const categoryIcons = {
   '전체': '/img/category/all.png',
-  'AI 개발': '/img/category/AI.png',
-  'AI 활용(AX)': '/img/category/AI 활용.png',
-  '개발·프로그래밍': '/img/category/개발, 프로그래밍.png',
-  '게임 개발': '/img/category/게임 개발.png',
-  '데이터 사이언스': '/img/category/데이터 사이언스.png',
-  '보안·네트워크': '/img/category/보안 네트워크.png',
-  '하드웨어': '/img/category/하드웨어.png',
-  '디자인·아트': '/img/category/디자인 아트.png',
-  '기획·경영·마케팅': '/img/category/기획 경영 마케팅.png',
-  '모의면접 or 취준 관련': '/img/category/모의면접 or 취준 관련.png',
-  '이력서': '/img/category/이력서.png'
+  '경영': '/img/category/경영.png',
+  '경제': '/img/category/경제.png',
+  '교육': '/img/category/교육.png',
+  '기계': '/img/category/기계.png',
+  '디자인': '/img/category/디자인.png',
+  '법': '/img/category/법.png',
+  '생명': '/img/category/생명.png',
+  '전기·전자': '/img/category/전기·전자.png',
+  '컴공': '/img/category/컴공.png',
+  '화학': '/img/category/화학.png',
+  '면접': '/img/category/면접.png'
 };
 
 const CategoryMenu = () => {
