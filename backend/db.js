d38bc4e01103905f541b2100fe93b6906333dfad
ym529 <yemyeong0529@naver.com>
2025-11-19 11:11:55 +0900

    커뮤니티 게시글 창 수정 및 로고 클릭 시 메인으로

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: '09080908', // MySQL 비밀번호
+  password: 'charming0218', // MySQL 비밀번호
   database: 'mentoring'
 });
 
