cb6b41614d30d8bed053fccb98c1b834141a3c95
hominlll <simbalhm@gmail.com>
2025-12-07 21:39:29 +0900

    디테일페이지 삭제버튼 수정

--- frontend/src/pages/StudyDetailPage.js
+++ frontend/src/pages/StudyDetailPage.js
@@ -178,7 +178,7 @@ const StudyDetailPage = ({ currentUserId, userNickname }) => {
           ← 뒤로가기
         </button>
 
-        {currentUser && study.userId === currentUser.userId && (
+        {currentUser && study.writer === currentUser.nickname && (
           <button
             className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-300"
             onClick={handleDelete}
